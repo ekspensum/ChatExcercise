a57f0f8a059f5696b49b5f423460e729b4c16053
Ekspensum <ekspensum@interia.pl>
2021-03-09 19:07:28 +0100

    ChatExcercise start

---
+++ src/main/java/pl/aticode/ChatExcerciseApplication.java
@@ -0,0 +1,13 @@
+package pl.aticode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatExcerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatExcerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/aticode/ChatExcerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.aticode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatExcerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
